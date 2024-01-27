fbb10181b4c09ffd756312762c06d56d8c4acbf8
joyal7701 <joyalshah07@gmail.com>
2024-01-27 18:56:33 -0500

    workflow done

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,11 @@ import bodyParser from "body-parser";
 import routes from "./routes/routes";
 
 const app = express();
-const port = 3000;
 
 app.use(bodyParser.json());
 
 app.use("/", routes);
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log(`Server is running at http://localhost:3000`);
 });
